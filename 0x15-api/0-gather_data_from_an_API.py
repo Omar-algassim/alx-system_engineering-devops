8afacd0522c9620a76ca76c5c3fc570d8dc6b0b7
Omar-Algassim <omarggg2@gamil.com>
2024-04-24 16:40:53 +0200

    file cant run when import

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,25 +5,26 @@
 import requests
 import sys
 
-user_id = int(sys.argv[1])
-url_todo = "https://jsonplaceholder.typicode.com/todos/"
-url_user = "https://jsonplaceholder.typicode.com/users" + "/" + sys.argv[1]
-task_num = 0
-done = 0
-task_name = []
-response = requests.get(url=url_todo)
-user_name = requests.get(url=url_user)
-users = user_name.json()
-data = response.json()
+if __name__ == "__main__":
+    user_id = int(sys.argv[1])
+    url_todo = "https://jsonplaceholder.typicode.com/todos/"
+    url_user = "https://jsonplaceholder.typicode.com/users" + "/" + sys.argv[1]
+    task_num = 0
+    done = 0
+    task_name = []
+    response = requests.get(url=url_todo)
+    user_name = requests.get(url=url_user)
+    users = user_name.json()
+    data = response.json()
 
-name = users['name']
-for dicts in data:
-    if user_id == dicts['userId']:
-        task_num += 1
-        if dicts['completed'] is True:
-            task_name.append(dicts['title'])
-            done += 1
+    name = users['name']
+    for dicts in data:
+        if user_id == dicts['userId']:
+            task_num += 1
+            if dicts['completed'] is True:
+                task_name.append(dicts['title'])
+                done += 1
 
-print(f"Employee {name} is done with tasks({done}/{task_num}):")
-for task in task_name:
-    print(f"\t {task}")
+    print(f"Employee {name} is done with tasks({done}/{task_num}):")
+    for task in task_name:
+        print(f"\t {task}")
