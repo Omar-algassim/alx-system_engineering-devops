11570f1d9587ce7b33cd19657dd0605f2d6d93fc
Omar-Algassim <omarggg2@gamil.com>
2024-05-08 00:46:45 +0300

    retrun number of subscibers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,14 +2,16 @@
 """get the number of all subscriber in subreddit"""
 
 import requests
-import os
 
 
 def number_of_subscribers(subreddit):
     """ get number of subscriber"""
-    url = f"https://www.reddit.com/r/{subreddit}/about"
-    respons = requests.get(url=url, allow_redirects=False)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
+        (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+    respons = requests.get(url=url, allow_redirects=False,
+                           headers={"User-Agent": user_agent})
     if respons.status_code == 404:
         return 0
-    data = respons.json()
-    return data.subscribers
+    data = respons.json().get("data")
+    return data.get("subscribers")
