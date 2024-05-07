b4d2d265bfed481b849a44f8b5086441c295e8d7
Omar-Algassim <omarggg2@gamil.com>
2024-05-08 01:15:39 +0300

    top ten post

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""get the number of all subscriber in subreddit"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """ get number of subscriber"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
+        (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+    paramater = {"limit": 10}
+    respons = requests.get(url=url, allow_redirects=False,
+                           headers={"User-Agent": user_agent},
+                           params=paramater)
+    if respons.status_code == 404:
+        print("None")
+    data = respons.json().get("data")
+    [print(titles.get("data").get("title")) for titles in data.get("children")]
